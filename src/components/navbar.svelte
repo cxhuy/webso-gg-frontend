27e802ea359e0838135e81ce804c368703f59d11
cxhuy <alvinkang1@gmail.com>
2022-07-26 22:58:47 +0900

    add background

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -6,7 +6,7 @@
 
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
 	<div class="container flex flex-wrap justify-between items-center mx-auto">
-		<a href="https://flowbite.com/" class="flex items-center">
+		<a href="/" class="flex items-center">
 			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
 				>웹소지지</span
 			>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,9 @@
     import "../app.css";
     import Navbar from "../components/navbar.svelte";
 </script>
-<div class="container mx-auto lg:w-8/12">
-    <Navbar/>
+<div class="m-0 h-screen w-screen dark:bg-gray-900">
+    <div class="container mx-auto lg:w-8/12">
+        <Navbar/>
+    </div>
 </div>
 <slot />
