139c0cbaa596acd0bea5af8828577ac14961094b
cxhuy <alvinkang1@gmail.com>
2022-07-27 15:58:02 +0900

    platformInfo now uses props

---
+++ src/routes/platforms/munpia.svelte
@@ -0,0 +1,15 @@
+<script>
+    import PlatformInfo from "../../components/platform/platform-info.svelte";
+
+    let platformInfoData = {
+        platformName: "문파아",
+        platformYear: 2012,
+        platformMonth: 12,
+        totalViews: 123456789,
+        totalNovels: 1234,
+        avgChapters: 123,
+        avgPeriod: 1.2,
+    }
+</script>
+
+<PlatformInfo platformInfoData={platformInfoData}/>
