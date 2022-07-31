936f9a2c5568577d09b2bf5402b11daa784b50d0
cxhuy <alvinkang1@gmail.com>
2022-07-31 19:59:25 +0900

    add different layout for mobile

--- src/components/platform/genreTable.svelte
+++ src/components/platform/genreTable.svelte
@@ -12,30 +12,52 @@
 </script>
 
 <div class="container px-3 py-2.5 mt-6">
-	<div>
-		<span class="text-3xl font-light dark:text-gray-200">장르별 통계</span>
-	</div>
-	<table class="mt-3 w-full table-auto font-light dark:text-gray-200">
-		<thead class="bg-black bg-opacity-10 dark:bg-white dark:bg-opacity-10">
-			<tr class="text-left">
-				<th class="p-3 font-normal">장르</th>
-                <th class="p-3 font-normal">작품수</th>
-                <th class="p-3 font-normal">조회수</th>
-                <th class="p-3 font-normal">평균 조회수</th>
-                <th class="p-3 font-normal">좋아요 수</th>
-                <th class="p-3 font-normal">평균 좋아요 수</th>
-                <th class="p-3 font-normal">선작수</th>
-                <th class="p-3 font-normal">평균 선작 수</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each genreData as keyword}
-				<tr class="bg-black bg-opacity-5 dark:bg-white dark:bg-opacity-5">
-					{#each keyword as data}
-						<td class="p-3">{data.toLocaleString()}</td>
-					{/each}
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+    <div>
+        <span class="text-3xl font-light dark:text-gray-200">장르별 통계</span>
+    </div>
+    <div class="hidden lg:block">
+        <table class="mt-3 w-full table-auto font-light dark:text-gray-200">
+            <thead class="bg-black bg-opacity-10 dark:bg-white dark:bg-opacity-10">
+                <tr class="text-left">
+                    <th class="p-3 font-normal">장르</th>
+                    <th class="p-3 font-normal">작품수</th>
+                    <th class="p-3 font-normal">조회수</th>
+                    <th class="p-3 font-normal">평균 조회수</th>
+                    <th class="p-3 font-normal">좋아요 수</th>
+                    <th class="p-3 font-normal">평균 좋아요 수</th>
+                    <th class="p-3 font-normal">선작수</th>
+                    <th class="p-3 font-normal">평균 선작수</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each genreData as genre}
+                    <tr class="bg-black bg-opacity-5 dark:bg-white dark:bg-opacity-5">
+                        {#each genre as data}
+                            <td class="p-3">{data.toLocaleString()}</td>
+                        {/each}
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+    <div class="lg:hidden">
+        {#each genreData as genre}
+            <div class="mt-3 p-3 font-light rounded-md dark:text-gray-200 bg-black bg-opacity-5 dark:bg-white dark:bg-opacity-5">
+                <div class="text-xl">{genre[0]}</div>
+                <div class="mt-auto ml-auto">{genre[1].toLocaleString()} 작품</div>
+                <div class="mt-2 grid grid-cols-2">
+                    <div><span class="text-gray-600 dark:text-gray-400">조회수: </span>{genre[2].toLocaleString()}</div>
+                    <div class="ml-4"><span class="text-gray-600 dark:text-gray-400">평균 조회수: </span>{genre[3].toLocaleString()}</div>
+                </div>
+                <div class="grid grid-cols-2">
+                    <div><span class="text-gray-600 dark:text-gray-400">좋아요 수: </span>{genre[2].toLocaleString()}</div>
+                    <div class="ml-4"><span class="text-gray-600 dark:text-gray-400">평균 좋아요 수: </span>{genre[3].toLocaleString()}</div>
+                </div>
+                <div class="grid grid-cols-2">
+                    <div><span class="text-gray-600 dark:text-gray-400">선작수: </span>{genre[2].toLocaleString()}</div>
+                    <div class="ml-4"><span class="text-gray-600 dark:text-gray-400">평균 선작수: </span>{genre[3].toLocaleString()}</div>
+                </div>
+            </div>
+        {/each}
+    </div>
 </div>
