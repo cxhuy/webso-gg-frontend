49a80170003da6d5602fd6b3924d84e3a5d1cf22
cxhuy <alvinkang1@gmail.com>
2022-08-03 20:50:34 +0900

    finish platform pages

---
+++ src/routes/platforms/navernovel.svelte
@@ -0,0 +1,13 @@
+<script>
+    import PlatformInfo from "../../components/platform/platform-info.svelte";
+    import Heatmap from "../../components/platform/heatmap.svelte"
+    import GenreTable from "../../components/platform/genreTable.svelte";
+    import UploadPeriodTable from "../../components/platform/uploadPeriodTable.svelte";
+
+    import NavernovelData from "../../jsondata/navernovel.json"
+</script>
+
+<PlatformInfo platformInfoData={NavernovelData.platformInfoData}/>
+<Heatmap heatmapData={NavernovelData.heatmapData} platformPricings={NavernovelData.platformInfoData.platformPricings} platformGenres={NavernovelData.platformInfoData.platformGenres}/>
+<GenreTable genreData={NavernovelData.genreData}/>
+<UploadPeriodTable weeklyUploadCountData={NavernovelData.weeklyUploadCountData}/>
