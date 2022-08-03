28737578315af48ecc9344e2f7a4f64fe620f5df
cxhuy <alvinkang1@gmail.com>
2022-08-04 03:04:50 +0900

    add footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,14 @@
 <script>
     import "../app.css";
     import Navbar from "../components/navbar.svelte";
+    import Footer from "../components/footer.svelte";
 </script>
 
 <div class="m-0 h-min w-min xs:w-screen dark:bg-gray-900">
     <div class="container mx-auto lg:w-10/12 xl:w-8/12">
         <Navbar/>
         <slot />
+        <Footer/>
     </div>
 </div>
 
